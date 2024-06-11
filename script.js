513d338486ad6464f4d044765a3a257fe1c611e8
roxwal <163902062+roxwal@users.noreply.github.com>
2024-06-12 01:39:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  const audio = document.querySelector('audio');
+  const body = document.body;
+
+  audio.addEventListener('play', () => {
+    body.classList.add('play-music');
+  });
+
+  audio.addEventListener('pause', () => {
+    body.classList.remove('play-music');
+  });
+});
